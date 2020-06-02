7978698478996009c66d095cf32b1d1e95b833a6
nipun marak <foresightbyte20@gmail.com>
2020-06-02 17:01:43 +0600

    Update June 2nd
    Chat connect with one room only, pop up msg on chat

--- CareGiverIonicProject/src/app/home/chat/home/home.page.ts
+++ CareGiverIonicProject/src/app/home/chat/home/home.page.ts
@@ -2,98 +2,99 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import * as firebase from 'Firebase';
 import {ActivatedRoute, NavigationExtras, Router} from '@angular/router';
 import {IonContent} from '@ionic/angular';
+import { AuthService } from 'src/app/auth.service';
+import { UserService } from 'src/app/user.service';
 
 export const snapshotToArray = snapshot => {
-    const returnArr = [];
+  const returnArr = [];
 
-    snapshot.forEach(childSnapshot => {
-        const item = childSnapshot.val();
-        item.key = childSnapshot.key;
-        returnArr.push(item);
-    });
-    return returnArr;
+  snapshot.forEach(childSnapshot => {
+    const item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+  return returnArr;
 };
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.page.html',
-    styleUrls: ['./home.page.scss'],
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 
 export class HomePage implements OnInit {
 
-    data = {type: '', displayName: '', message: ''};
-    chats: any = [];
-    roomkey: any = null;
-    displayName: string = null;
-    offStatus: boolean;
-    @ViewChild(IonContent, {static: false}) content: IonContent;
+  data = { type: '', displayName: '',  message: '' };
+  chats: any = [];
+  roomkey: any = null;
+  displayName: string = null;
+  offStatus: boolean;
+  @ViewChild(IonContent, {static: false}) content: IonContent;
 
-    constructor(
-        private Aroute: ActivatedRoute,
-        private router: Router) {
-        this.Aroute.queryParams.subscribe(params => {
-            if (params && params.roomKey) {
-                this.roomkey = JSON.parse(params.roomKey);
-                this.displayName = JSON.parse(params.displayName);
-            }
+  constructor(
+    private Aroute: ActivatedRoute,
+    private router: Router,
+    public user: UserService,
+    private auth: AuthService  ) {
+      const uid = this.auth.cUid;
+      this.displayName = this.user.displayName;
+      this.roomkey = 'chatroom';
+      console.log(this.displayName);
 
-            this.offStatus = false;
-            this.data.type = 'message';
-            this.data.displayName = this.displayName;
+      this.offStatus = false;
+      this.data.type = 'message';
+      this.data.displayName = this.displayName;
 
-            const joinData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
-            joinData.set({
-                type: 'join',
-                user: this.displayName,
-                message: this.displayName + ' has joined this room.',
-                sendDate: Date()
-            });
-            this.data.message = '';
+      const joinData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
+      joinData.set({
+      type: 'join',
+      user: this.displayName,
+      message: this.displayName + ' has joined this room.',
+      sendDate: Date()
+      });
+      this.data.message = '';
 
-            firebase.database().ref('chatrooms/' + this.roomkey + '/chats').on('value', resp => {
-                this.chats = [];
-                this.chats = snapshotToArray(resp);
-                console.log(this.chats);
-                setTimeout(() => {
-                    if (this.offStatus === false) {
-                        this.content.scrollToBottom(300);
-                    }
-                }, 1000);
-            });
-        });
-    }
-
-    ngOnInit() {
-    }
-
-    sendMessage() {
-        const newData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
-        newData.set({
-            type: this.data.type,
-            user: this.data.displayName,
-            message: this.data.message,
-            sendDate: Date()
-        });
-        this.data.message = '';
-    }
-
-    exitChat() {
-        const exitData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
-        exitData.set({
-            type: 'exit',
-            user: this.displayName,
-            message: this.displayName + ' has exited this room.',
-            sendDate: Date()
-        });
-        this.offStatus = true;
-
-        const navExtras: NavigationExtras = {
-            queryParams: {
-                displayName: this.displayName
-            }
-        };
-        this.router.navigate(['room'], navExtras);
-    }
+      firebase.database().ref('chatrooms/' + this.roomkey + '/chats').on('value', resp => {
+      this.chats = [];
+      this.chats = snapshotToArray(resp);
+      console.log(this.chats);
+      setTimeout(() => {
+        if (this.offStatus === false) {
+        this.content.scrollToBottom(300);
+      }
+    }, 1000);
+  });
 }
 
+  ngOnInit() {}
+
+  sendMessage() {
+    const newData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
+    newData.set({
+      type: this.data.type,
+      user: this.data.displayName,
+      message: this.data.message,
+      sendDate: Date()
+    });
+    this.data.message = '';
+  }
+
+  exitChat() {
+    const exitData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
+    exitData.set({
+      type: 'exit',
+      user: this.displayName,
+      message: this.displayName + ' has exited this room.',
+      sendDate: Date()
+    });
+    this.offStatus = true;
+
+    const navExtras: NavigationExtras = {
+      queryParams: {
+        displayName: this.displayName
+      }
+    };
+    this.router.navigate(['/'], navExtras);
+  }
+
+}
--- CareGiverIonicProject/src/app/home/tabs/tabs.page.ts
+++ CareGiverIonicProject/src/app/home/tabs/tabs.page.ts
@@ -1,56 +1,68 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../auth.service';
 import {LoginComponent} from '../../login/login.component';
-import {ActionSheetController} from '@ionic/angular';
+import {ActionSheetController, AlertController} from '@ionic/angular';
 import {Router} from '@angular/router';
 import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
-    selector: 'app-tabs',
-    templateUrl: './tabs.page.html',
-    styleUrls: ['./tabs.page.scss'],
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
 
-    constructor(
-        private router: Router,
-        public afAuth: AngularFireAuth,
-        public Uauth: AuthService,
-        public log: LoginComponent,
-        public actionSheetController: ActionSheetController
-    ) {
+  constructor(
+    private router: Router,
+    public afAuth: AngularFireAuth,
+    public Uauth: AuthService,
+    public log: LoginComponent,
+    private alert: AlertController,
+    public actionSheetController: ActionSheetController
+    ) { }
+
+  ngOnInit() {
+  }
+
+  async overlap() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Tools',
+      cssClass: 'EditIcon',
+      buttons: [{
+      text: 'Profile',
+      role: 'information',
+      icon: 'contact',
+      handler: () => {
+      //  return this.router.navigate(['./profile']);
+        }
+      }, {
+      text: 'Settings',
+      role: 'settings',
+      icon: 'settings',
+      handler: () => {
+      //  return this.router.navigate(['./settings']);
+        }
+      }, {
+      text: 'Sign Out',
+      icon: 'power',
+      handler: () => {
+      this.afAuth.auth.signOut();
+      return this.router.navigate(['/tabs']);
+        }
+      }]
+      });
+    await actionSheet.present();
     }
 
-    ngOnInit() {
-    }
-
-    async overlap() {
-        const actionSheet = await this.actionSheetController.create({
-            header: 'Tools',
-            cssClass: 'EditIcon',
-            buttons: [{
-                text: 'Profile',
-                role: 'information',
-                icon: 'contact',
-                handler: () => {
-                    //  return this.router.navigate(['./profile']);
-                }
-            }, {
-                text: 'Settings',
-                role: 'settings',
-                icon: 'settings',
-                handler: () => {
-                    //  return this.router.navigate(['./settings']);
-                }
-            }, {
-                text: 'Sign Out',
-                icon: 'power',
-                handler: () => {
-                    this.afAuth.auth.signOut();
-                    return this.router.navigate(['/tabs']);
-                }
-            }]
-        });
-        await actionSheet.present();
-    }
+  popAlert() {
+    this.showAlert('Talk about your challenges');
+  }
+  // pop up alert message
+  async showAlert(message: string) {
+    const alert = this.alert.create({
+      message,
+      buttons: ['OK']
+    });
+    await (await alert).present();
+  }
 }
